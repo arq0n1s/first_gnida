411a98c2e3d6c3fa3f40501e616fba3106c9dda4
PavelliOn <113195573+arq0n1s@users.noreply.github.com>
2024-11-18 20:20:26 +0200

    changed db from sqlite3 to mysql

--- database/migrations/2024_10_28_204643_add_column_content_to_posts.php
+++ database/migrations/2024_10_28_204643_add_column_content_to_posts.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //$table->text('content')->nullable()->after('description');
+            $table->text('content')->nullable()->after('description');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //$table->dropColumn('content');
+            $table->dropColumn('content');
         });
     }
 };
